d6d203cf89464ca45d7a071b8a4ef78f636e48d6
Enes T <66560757+devenes@users.noreply.github.com>
2022-03-12 01:20:37 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
-from unicodedata import east_asian_width
-
-
-config = {"host": "0.0.0.0",
-          "port": 80,
-          'aws_access_key_id': 12312321321,
-          'aws_secret_access_key': 123312123123,
-          'region_name': 'us-east-1'
-          }
+config = {
+    "host": "0.0.0.0",
+    "port": 8080,
+    "log_file": "app.log",
+    "log_level": "INFO",
+    "key_name": "ssh1"
+}
