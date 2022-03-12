7d1ed36e2dd48e7ee1b3487b78c56d9f9c8cf44a
Enes T <66560757+devenes@users.noreply.github.com>
2022-03-12 04:00:41 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,107 @@
+from flask import Flask, request, jsonify
+from config import config
+import logging
+import boto3
+
+app = Flask(__name__)
+
+logging.basicConfig(
+    filename=config['log_file'],
+    level=config['log_level']
+)
+
+
+# Endpoint: http://<api_host>:<api_port>/ec2/list
+@app.route('/ec2/list', methods=['POST', 'GET'])
+def aws_list():
+    try:
+        aws_access_key_id = request.args.get("aws_access_key_id")
+        aws_secret_access_key = request.args.get("aws_secret_access_key")
+        region_name = request.args.get("region_name")
+        client = boto3.client('ec2',
+                              aws_access_key_id=aws_access_key_id,
+                              aws_secret_access_key=aws_secret_access_key,
+                              region_name=region_name
+                              )
+
+    except Exception as error:
+        print(str(error))
+        return jsonify({'Error': "Unexpected Error occured"}), 500
+
+    instances = client.describe_instances()
+    output = []
+    for reservation in instances['Reservations']:
+        output.extend([
+            {
+                'id': instance['InstanceId'],
+                "instance-type": instance['InstanceType'],
+                "instance-state": instance['State']['Name']
+            }
+            for instance in reservation['Instances']])
+
+    # print(output)
+    return jsonify(output), 200
+
+
+# Endpoint: http://<api_host>:<api_port>/ec2/start
+@app.route("/ec2/start", methods=["POST", "GET"])
+def start_ec2_instances():
+    try:
+        aws_access_key_id = request.args.get("aws_access_key_id")
+        aws_secret_access_key = request.args.get("aws_secret_access_key")
+        region_name = request.args.get("region_name")
+        InstanceIds = request.args.get("InstanceId")
+        client = boto3.client('ec2',
+                              aws_access_key_id=aws_access_key_id,
+                              aws_secret_access_key=aws_secret_access_key,
+                              region_name=region_name,
+                              )
+
+    except Exception as error:
+        print(str(error))
+        return jsonify({'Error': "Unexpected Error occured"}), 500
+
+    response = client.describe_instances()
+    InstanceIds = []
+    for instance in response["Reservations"][0]["Instances"]:
+        InstanceIds.append(instance["InstanceId"])
+
+    response = client.start_instances(
+        InstanceIds=InstanceIds
+    )
+
+    return jsonify(response["StartingInstances"][0]), 200
+
+
+# Endpoint: http://<api_host>:<api_port>/ec2/stop
+@app.route("/ec2/stop", methods=["POST", "GET"])
+def stop_ec2_instances():
+    try:
+        aws_access_key_id = request.args.get("aws_access_key_id")
+        aws_secret_access_key = request.args.get("aws_secret_access_key")
+        region_name = request.args.get("region_name")
+        InstanceIds = request.args.get("InstanceId")
+        client = boto3.client('ec2',
+                              aws_access_key_id=aws_access_key_id,
+                              aws_secret_access_key=aws_secret_access_key,
+                              region_name=region_name,
+                              )
+
+    except Exception as error:
+        print(str(error))
+        return jsonify({'Error': "Unexpected Error occured"}), 500
+
+    response = client.describe_instances()
+    InstanceIds = []
+    for instance in response["Reservations"][0]["Instances"]:
+        InstanceIds.append(instance["InstanceId"])
+
+    response = client.stop_instances(
+        InstanceIds=InstanceIds
+    )
+
+    return jsonify(response["StoppingInstances"][0]), 200
+
+
+if __name__ == "__main__":
+    app.run(host=config["host"], port=config["port"], debug=True)
